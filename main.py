8912f09832c6f80f8c18406f79af437e3e02f87a
Kai Piper <kai9987kai@gmail.com>
2023-06-27 22:29:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from torch import nn, optim
 from torch.nn import functional as F
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.circuit import Parameter
+import random
 
 # Create a quantum circuit with 2 qubits
 n_qubits = 2
@@ -21,9 +22,9 @@ qc.measure_all()
 
 # Define the quantum layer
 class QuantumLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, theta):
         super().__init__()
-        self.theta = nn.Parameter(torch.randn(1))
+        self.theta = nn.Parameter(torch.tensor(theta, dtype=torch.float))
 
     def forward(self, x):
         # Execute the quantum circuit on a simulator
@@ -40,54 +41,82 @@ class QuantumLayer(nn.Module):
 
 # Define the neural network
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, theta):
         super().__init__()
         self.fc1 = nn.Linear(4, 2)
-        self.quantum_layer = QuantumLayer()
+        self.quantum_layer = QuantumLayer(theta)
 
     def forward(self, x):
         x = self.quantum_layer(x)
         x = F.relu(self.fc1(x))
         return x
 
-# Create the neural network
-model = Net()
-
 # Define the bot
 class Bot:
-    def __init__(self, canvas, x, y):
+    def __init__(self, canvas, x, y, theta):
         self.canvas = canvas
         self.x = x
         self.y = y
         self.rect = canvas.create_rectangle(x, y, x+10, y+10, fill='blue')
+        self.model = Net(theta)
 
     def move(self):
         # Get the output of the neural network
-        output = model(torch.tensor([1.0]))
+        output = self.model(torch.tensor([1.0]))
 
         # Move the bot based on the output of the neural network
         dx = output[0].item()
         dy = output[1].item()
-        self.canvas.move(self.rect, dx, dy)
+
+        # Get the current position of the bot
+        pos = self.canvas.coords(self.rect)
+
+        # Check if the bot is within the area
+        if 0 <= pos[0] + dx <= 390 and 0 <= pos[1] + dy <= 390:
+            self.canvas.move(self.rect, dx, dy)
+
+        return dx
 
 # Create a Tkinter window
 root = tk.Tk()
 canvas = tk.Canvas(root, width=400, height=400)
 canvas.pack()
 
-# Create multiple bots
-bots = [Bot(canvas, 200, 200) for _ in range(10)]
+# Create multiple bots with random initial parameters
+bots = [Bot(canvas, 200, 200, random.random()) for _ in range(10)]
 
 # Move the bots every 100 milliseconds
 def update():
-    for bot in bots:
-        bot.move()
+    global bots
+
+    # Move each bot and calculate its fitness
+    fitnesses = [bot.move() for bot in bots]
+
+    # Normalize the fitnesses so that they sum to 1
+    total_fitness = sum(fitnesses)
+    normalized_fitnesses = [fitness / total_fitness for fitness in fitnesses]
+
+    # Create a new generation of bots
+    new_bots = []
+    for _ in range(len(bots)):
+        # Select two bots to be the parents of the new bot
+        parent1, parent2 = random.choices(bots, weights=normalized_fitnesses, k=2)
+
+        # Create the new bot by averaging the parameters of the parents
+        new_theta = (parent1.model.quantum_layer.theta.item() + parent2.model.quantum_layer.theta.item()) / 2
+        new_bot = Bot(canvas, 200, 200, new_theta)
+
+        # Add the new bot to the new generation
+        new_bots.append(new_bot)
+
+    # Replace the old generation with the new generation
+    bots = new_bots
+
+    # Schedule the next update
     root.after(100, update)
 
-# Create a thread for the update function
+# Start the update function in a new thread
 update_thread = threading.Thread(target=update)
-
-# Start the thread
 update_thread.start()
 
 # Start the Tkinter event loop
