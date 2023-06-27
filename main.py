f15530c01909ec4b7e94e9f7cf27f5aa86903eb0
Kai Piper <kai9987kai@gmail.com>
2023-06-27 22:18:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,97 @@
+import threading
+import tkinter as tk
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+from qiskit import QuantumCircuit, execute, Aer
+from qiskit.circuit import Parameter
+
+# Create a quantum circuit with 2 qubits
+n_qubits = 2
+qc = QuantumCircuit(n_qubits)
+
+# Apply Hadamard gate to the first qubit
+qc.h(0)
+
+# Apply a CNOT gate
+qc.cx(0, 1)
+
+# Measure the qubits
+qc.measure_all()
+
+# Define the quantum layer
+class QuantumLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.theta = nn.Parameter(torch.randn(1))
+
+    def forward(self, x):
+        # Execute the quantum circuit on a simulator
+        backend = Aer.get_backend('qasm_simulator')
+        job = execute(qc, backend, shots=1000, parameter_binds=[{Parameter('theta'): self.theta.item()}])
+        result = job.result()
+
+        # Get the counts of the result
+        counts = result.get_counts(qc)
+
+        # Convert the counts to a tensor
+        counts_tensor = torch.tensor([counts.get('00', 0), counts.get('01', 0), counts.get('10', 0), counts.get('11', 0)])
+        return counts_tensor
+
+# Define the neural network
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(4, 2)
+        self.quantum_layer = QuantumLayer()
+
+    def forward(self, x):
+        x = self.quantum_layer(x)
+        x = F.relu(self.fc1(x))
+        return x
+
+# Create the neural network
+model = Net()
+
+# Define the bot
+class Bot:
+    def __init__(self, canvas, x, y):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.rect = canvas.create_rectangle(x, y, x+10, y+10, fill='blue')
+
+    def move(self):
+        # Get the output of the neural network
+        output = model(torch.tensor([1.0]))
+
+        # Move the bot based on the output of the neural network
+        dx = output[0].item()
+        dy = output[1].item()
+        self.canvas.move(self.rect, dx, dy)
+
+# Create a Tkinter window
+root = tk.Tk()
+canvas = tk.Canvas(root, width=400, height=400)
+canvas.pack()
+
+# Create multiple bots
+bots = [Bot(canvas, 200, 200) for _ in range(10)]
+
+# Move the bots every 100 milliseconds
+def update():
+    for bot in bots:
+        bot.move()
+    root.after(100, update)
+
+# Create a thread for the update function
+update_thread = threading.Thread(target=update)
+
+# Start the thread
+update_thread.start()
+
+# Start the Tkinter event loop
+root.mainloop()
+
+# Wait for the update thread to finish
+update_thread.join()
