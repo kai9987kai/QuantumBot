903fff727983f31709dcb081d20aa1eb27b0eb74
Kai Piper <kai9987kai@gmail.com>
2023-06-29 19:20:27 +0100

    Update beta.py

--- beta.py
+++ beta.py
@@ -1,13 +1,14 @@
 import threading
-import queue
+
 import logging
 import random
-import turtle
+import time
+
 import math
 import tkinter as tk
 
 import torch
-from torch import nn, optim
+from torch import nn
 from torch.nn import functional as F
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.circuit import Parameter
@@ -45,13 +46,14 @@ qc.cx(0, 1)
 # Measure the qubits
 qc.measure_all()
 
+
 # Define the quantum layer
 class QuantumLayer(nn.Module):
     def __init__(self, theta):
         super().__init__()
         self.theta = nn.Parameter(torch.tensor(theta, dtype=torch.float))
 
-    def forward(self, x):
+    def forward(self):
         # Execute the quantum circuit on a simulator
         backend = Aer.get_backend('qasm_simulator')
         job = execute(qc, backend, shots=1, parameter_binds=[{Parameter('theta'): self.theta.item()}])
@@ -59,8 +61,10 @@ class QuantumLayer(nn.Module):
         counts = result.get_counts(qc)
 
         # Convert the counts to a tensor
-        counts_tensor = torch.tensor([counts.get('00', 0), counts.get('01', 0), counts.get('10', 0), counts.get('11', 0)], dtype=torch.float)
+        counts_tensor = torch.tensor(
+            [counts.get('00', 0), counts.get('01', 0), counts.get('10', 0), counts.get('11', 0)], dtype=torch.float)
         return counts_tensor
+
 
 # Define the neural network
 class NeuralNetwork(nn.Module):
@@ -68,7 +72,7 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.fc1 = nn.Linear(num_inputs, num_hidden)
         self.quantum_layer = QuantumLayer(theta)
-        self.fc2 = nn.Linear(num_hidden, num_hidden)  # Updated to num_hidden
+        self.fc2 = nn.Linear(num_hidden, num_outputs)  # Updated to num_outputs
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -76,13 +80,14 @@ class NeuralNetwork(nn.Module):
         x = F.relu(self.fc2(x))  # Updated to use the second linear layer
         return x
 
+
 # Define the bot
 class Bot:
-    def __init__(self, x, y, canvas, theta):
+    def __init__(self, x, y, canvi, theta):
         self.x = x
         self.y = y
-        self.canvas = canvas
-        self.rect = canvas.create_rectangle(x, y, x+10, y+10, fill='blue')
+        self.canvas = canvi
+        self.rect = canvi.create_rectangle(x, y, x + 10, y + 10, fill='blue')
         self.model = NeuralNetwork(theta)
         self.resources = []
         self.threats = []
@@ -107,8 +112,6 @@ class Bot:
         self.y += dy
         self.rect = self.canvas.create_rectangle(self.x, self.y, self.x + 10, self.y + 10, fill='blue')
 
-        return abs(dx)  # Reward is based on the absolute distance moved
-
     def share_resources(self, other_bots):
         for other in other_bots:
             if other != self and self.distance(other) < 100:
@@ -124,6 +127,7 @@ class Bot:
     def distance(self, other):
         return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
 
+
 # Create multiple bots with random initial parameters
 bots = [Bot(random.randint(0, 790), random.randint(0, 790), canvas, random.random()) for _ in range(10)]
 
@@ -137,53 +141,34 @@ threats = [Bot(random.randint(0, 790), random.randint(0, 790), canvas, random.ra
 for threat in threats:
     threat.rect = canvas.create_rectangle(threat.x, threat.y, threat.x + 10, threat.y + 10, fill='red')
 
-# Move the bots every 100 milliseconds
-def update():
-    global bots
 
-    # Move each bot and calculate its fitness
-    fitnesses = []
-    surviving_bots = []
-    for bot in bots:
-        dx = bot.move()
-        fitnesses.append(dx)
-        surviving_bots.append(bot)
+# Move the bots indefinitely
+def move_bots():
+    while True:
+        for bot in bots:
+            bot.move()
+        time.sleep(0.1)
 
-    # Normalize the fitnesses so that they sum to 1
-    total_fitness = sum(fitnesses)
-    if total_fitness == 0:
-        return
-    normalized_fitnesses = [fitness / total_fitness for fitness in fitnesses]
 
-    # Create a new generation of bots
-    new_bots = []
-    for _ in range(len(surviving_bots)):
-        # Select two bots to be the parents of the new bot
-        parent1, parent2 = random.choices(surviving_bots, weights=normalized_fitnesses, k=2)
+# Start the bot movement thread
+move_thread = threading.Thread(target=move_bots)
+move_thread.start()
 
-        # Create the new bot by averaging the parameters of the parents
-        new_theta = (parent1.model.quantum_layer.theta.item() + parent2.model.quantum_layer.theta.item()) / 2
-        new_bot = Bot(random.randint(0, 790), random.randint(0, 790), canvas, new_theta)
 
-        # Add the new bot to the new generation
-        new_bots.append(new_bot)
+# Share resources and threats among bots
+def share_resources_and_threats():
+    while True:
+        for bot in bots:
+            bot.share_resources(bots)
+            bot.share_resources(plants)
+            bot.share_threats(bots)
+            bot.share_threats(threats)
+        time.sleep(1)
 
-    # Replace the old generation with the new generation
-    bots = new_bots
 
-    # Share resources and threats among bots
-    for bot in bots:
-        bot.share_resources(bots)
-        bot.share_resources(plants)
-        bot.share_threats(bots)
-        bot.share_threats(threats)
-
-    # Schedule the next update
-    root.after(100, update)
-
-# Start the update function in a new thread
-update_thread = threading.Thread(target=update)
-update_thread.start()
+# Start the sharing thread
+share_thread = threading.Thread(target=share_resources_and_threats)
+share_thread.start()
 
 # Start the main loop
 root.mainloop()
